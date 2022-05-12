49a1af42729489bfc198856a190b2672c1cd4110
jacob sullivan <puremight01@gmail.com>
2022-05-12 20:26:03 +1200

    added ability system

--- Source/VerticalSlice/VerticalSlice.Build.cs
+++ Source/VerticalSlice/VerticalSlice.Build.cs
@@ -8,6 +8,6 @@ public class VerticalSlice : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemSteam", "GameplayTags", "GameplayAbilities", "GameplayTasks" });
 	}
 }
