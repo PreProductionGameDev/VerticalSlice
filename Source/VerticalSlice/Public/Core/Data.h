8c30d0b09e353fb67e046e267482f17fcdfb0b26
TheEpicStef <stefanpetrie.1@gmail.com>
2022-05-23 09:53:53 +1200

    Removed Element Enum data and moved to the Data.h

--- Source/VerticalSlice/Public/Core/Data.h
+++ Source/VerticalSlice/Public/Core/Data.h
@@ -9,3 +9,12 @@ enum class EPlayerAbilityInputID : uint8
 	PrimaryAction,
 	SecondaryAction
 };
+
+UENUM(BlueprintType)
+enum class EElements : uint8
+{
+	Elem_Fire		UMETA(DisplayName = "Fire"),
+	Elem_Air		UMETA(DisplayName = "Air"),
+	Elem_Ice		UMETA(DisplayName = "Ice"),
+	Elem_Earth		UMETA(DisplayName = "Earth")
+};
