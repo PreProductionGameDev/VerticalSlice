953e64b215c2969aaa2d2106ffe29ee52c8b821b
jacob sullivan <puremight01@gmail.com>
2022-06-13 12:29:09 +1200

    reverted changes

--- Source/VerticalSlice/VerticalSlice.Build.cs
+++ Source/VerticalSlice/VerticalSlice.Build.cs
@@ -9,5 +9,6 @@ public class VerticalSlice : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemSteam", "GameplayTags", "GameplayAbilities", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Blutility"});
 	}
 }
