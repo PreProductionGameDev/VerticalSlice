f53867490b8fbbab8cfa972ef552c3ef16af6332
Stef <theepicstef@users.noreply.github.com>
2022-10-01 16:08:15 +1300

    Moved GEDamageExecutionCalc
    To fit with File Formatting

--- Source/VerticalSlice/Private/Core/Abilities/GameplayEffects/GEDamageExecutionCalc.cpp
+++ Source/VerticalSlice/Private/Core/Abilities/GameplayEffects/GEDamageExecutionCalc.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Core/Abilities/GEDamageExecutionCalc.h"
+#include "Core/Abilities/GameplayEffects/GEDamageExecutionCalc.h"
 #include "Core/PlayerAttributeSet.h"
 #include "Core/PlayerAbilitySystemComponent.h"
 #include "Core/Abilities/Utility/GEUIData_KillFeed.h"
