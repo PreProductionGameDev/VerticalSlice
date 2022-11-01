0d3383c67c3cd2f44c95ae80b463e375461ca3ea
Stefan <stefanpetrie.1@gmail.com>
2022-11-01 14:23:19 +1300

    Shotgun has all pellet hitmarkers

--- Source/VerticalSlice/Private/Core/Weapons/WeaponUtilityFunctionLibrary.cpp
+++ Source/VerticalSlice/Private/Core/Weapons/WeaponUtilityFunctionLibrary.cpp
@@ -12,33 +12,38 @@
  */
 void UWeaponUtilityFunctionLibrary::CheckHitMarker(const FGameplayAbilityTargetDataHandle& TargetData, AFP_Character* OwningPlayer)
 {
-	const FHitResult HitResult = UAbilitySystemBlueprintLibrary::GetHitResultFromTargetData(TargetData, 0);
+	for (int i = 0; i < UAbilitySystemBlueprintLibrary::GetDataCountFromTargetData(TargetData); i++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HITMARKER GOBBBBBBBBBBBBBBBBRRRRRRRRRRR"));
+		const FHitResult HitResult = UAbilitySystemBlueprintLibrary::GetHitResultFromTargetData(TargetData, i);
 
-	// Get And Validate the Hit Player and their ASC
-	AFP_Character* HitPlayer = Cast<AFP_Character>(HitResult.GetActor());
-	if (!HitPlayer) return;
-	const UAbilitySystemComponent* HitPlayerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitPlayer);
-	if (!HitPlayerASC) return;
+		// Get And Validate the Hit Player and their ASC
+		AFP_Character* HitPlayer = Cast<AFP_Character>(HitResult.GetActor());
+		if (!HitPlayer) continue;
+		const UAbilitySystemComponent* HitPlayerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitPlayer);
+		if (!HitPlayerASC) continue;
 
-	// Get the Source Player's ASC
-	const UAbilitySystemComponent* SourcePlayerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OwningPlayer);
-	if (!SourcePlayerASC) return;
+		// Get the Source Player's ASC
+		const UAbilitySystemComponent* SourcePlayerASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OwningPlayer);
+		if (!SourcePlayerASC) continue;
 
-	// Return if the Player is dead
-	if (HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Dead")))) return;
+		// Return if the Player is dead
+		if (HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Dead")))) return;
 
-	// Return if both players are Blue team
-	if (HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Blue"))) &&
-		SourcePlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Blue"))))
-			return;
+		// Return if both players are Blue team
+		if (HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Blue"))) &&
+			SourcePlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Blue"))))
+				continue;
 
-	// Return if both players are Red Team
-	if (HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Red"))) &&
-		SourcePlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Red"))))
-			return;
+		// Return if both players are Red Team
+		if (HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Red"))) &&
+			SourcePlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("Team.Red"))))
+				continue;
 
-	// Check if player has entered the dying state
-	const bool HitPlayerIsDying = HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Dying")));
-	// Create Hit Marker on the owning player
-	OwningPlayer->CreateHitMarker(HitPlayerIsDying);
+		// Check if player has entered the dying state
+		const bool HitPlayerIsDying = HitPlayerASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Dying")));
+		// Create Hit Marker on the owning player
+		OwningPlayer->CreateHitMarker(HitPlayerIsDying);
+		UE_LOG(LogTemp, Warning, TEXT("OwO"));
+	}
 }
